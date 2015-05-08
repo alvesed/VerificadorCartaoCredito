bc2bd4c830586a51517ae1a6adf64ded2d2d5fe3
Edson F. Alves <alvesed@gmail.com>
2015-05-08 11:52:52 -0300

    First Commit
    Adicionando projeto ao GITHUB

---
+++ VerificadorCartaoCredito/VerificadorCartaoCredito/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VerificadorCartaoCredito.MvcApplication" Language="C#" %>
---
+++ VerificadorCartaoCredito/VerificadorCartaoCredito/Models/CartaoCredito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VerificadorCartaoCredito.Models
+{
+    public class CartaoCredito
+    {
+        //[Display(Name = "Número do Cartão de Crédito")]
+        public String NumeroCartaoCredito { get; set; }
+
+    }
+
+
+}
